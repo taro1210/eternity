c0b24266e25ea1a63cf343122de6b5e0b9023d45
taro1210 <42398914+taro1210@users.noreply.github.com>
2018-08-17 15:56:42 +0900

    見本

---
+++ BigOrSmall/CardMark.java
@@ -0,0 +1,55 @@
+package BigOrSmall;
+
+
+/**
+ * カードマーク
+ */
+public enum CardMark {
+	/**
+	 *  カードのマーク クラブ
+	 */
+	Clubs("クラブ", 0),
+	/**
+	 *  カードのマーク ダイヤ
+	 */
+	Diamond("ダイヤ", 1),
+	/**
+	 *  カードのマーク ハート
+	 */
+	Heart("ハート", 2),
+	/**
+	 *  カードのマーク スペード
+	 */
+	Spade("スペード", 3);
+
+	/* マークの名前 */
+	private String markName;
+	/* マークのレベル（強さ) */
+	private int level;
+
+	/**
+	 * コンストラクタ
+	 * @param markName マークの名前
+	 * @param level マークのレベル
+	 */
+	CardMark(String markName, int level) {
+		this.markName = markName;
+		this.level = level;
+	}
+
+	/**
+	 * マークの名前を取得
+	 * @return
+	 */
+	public String getMarkName() {
+		return this.markName;
+	}
+
+	/**
+	 * マークのレベルを取得
+	 * @return
+	 */
+	public int getLevel() {
+		return this.level;
+	}
+}
