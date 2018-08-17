3c0b1e3aa092a3d1ae16cf212e6c022a3786146d
taro1210 <42398914+taro1210@users.noreply.github.com>
2018-08-17 16:59:19 +0900

    Add files via upload

--- Lise15_8/Main.java
+++ Lise15_8/Main.java
@@ -1,13 +1,26 @@
 package Lise15_8;
 
+import java.io.StreamCorruptedException;
+
 public class Main {
-	Math math=new Math();
-	public static void main(String[]args){
+	public static void main ( String [] args) {
+		String errerMesege=new String();
 		System.out.print("数値データを入力してください>");
-		try{
-		int math=new java.util.Scanner(System.in).nextInt();
-		}catch(java.util.InputMismatchException e){
+		try {
+		int imputval=new java.util.Scanner(System.in).nextInt();
+
+		  if ( imputval < 0) {
+			throw  new 	StreamCorruptedException ( errerMesege="入力されたデータが下限値をを下回っています。​");
+
+		} if ( imputval > 9) {
+			throw  new 	StreamCorruptedException ( errerMesege="入力されたデータが上限値をを上回っています。​");
+		}
+		System.out.print("正常に処理が行われました");
+		} catch ( java.util.InputMismatchException e ) {
 			System.out.println("半角数字1桁を入力して下さい");
+
+		} catch ( 	StreamCorruptedException e ) {
+			System.out.println(errerMesege);
 		}
 	}
 }
