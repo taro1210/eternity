45556b948f0fc38cb9f31a9649a7d9f50d880bbd
taro1210 <42398914+taro1210@users.noreply.github.com>
2018-08-20 12:54:34 +0900

    8/20

--- List14_4_3/Book.java
+++ List14_4_3/Book.java
@@ -1,16 +1,56 @@
 package List14_4_3;
 
-class Book{
+class Book {
 String name; // 書名
 int price; // 著者
 String author; // 著者
 
-Book(String name, int price, String author){
-	this.name=name;
-	this.price=price;
-	this.author=author;
+Book (String name, int price, String author) {
+	this.name = name;
+	this.price = price;
+	this.author = author;
 }
-public String toString(){
-return ("「書名:" + name +"/価格:" +price + "円/著者:" + author+"」");
-}
+	@Override  //??
+	public String toString() {
+	return ("「書名:" + name +"/価格:" +price + "円/著者:" + author+"」");
+	}
+	/* (非 Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + price;
+		return result;
+	}
+	/* (非 Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (price != other.price)
+			return false;
+		return true;
+	}
+
 }
--- List14_4_3/BookShelf.java
+++ List14_4_3/BookShelf.java
@@ -3,96 +3,107 @@ package List14_4_3;
 public class BookShelf{
 
 //本の追加
-Book[]hondana=new Book[10];
+Book[]hondana=new Book[5];
 int index=0;
 int x=0;
 
 public void end(){
-	System.out.println("終了します。");
+	System.out.println ("終了します。");
 }
 
 public void hontuika(){
 System.out.println("書籍を追加しますか？[はい: yes いいえ: no ]");
-String tuika=new java.util.Scanner(System.in).nextLine();
-	if(tuika.equals("yes")){
+String tuika = new java.util.Scanner(System.in).nextLine();
+	if (tuika.equals("yes")) {
 		tuika();
-	}else{
-
+	} else {
+		end();
 	}
 }
 
 public void tuika(){
-		System.out.print("書籍名を入力＞");
-		 String name=new java.util.Scanner(System.in).nextLine();
-		 System.out.print("価格を入力＞");
-		int price=new java.util.Scanner(System.in).nextInt();
-		System.out.print("著者名を入力＞");
-		 String author=new java.util.Scanner(System.in).nextLine();
-		 Book hon=new Book(name,price,author);
-		 System.out.println(hon+"を追加します。");
+		System.out.print ("書籍名を入力＞");
+		String name = new java.util.Scanner (System.in) .nextLine();
+		System.out.print ("価格を入力＞");
+		int price = new java.util.Scanner (System.in) .nextInt();
+		System.out.print ("著者名を入力＞");
+		String author = new java.util.Scanner (System.in).nextLine();
+		Book hon = new Book (name,price,author);
+		System.out.println (hon+"を追加します。");
 
-		 for(int i=0; i<hondana.length; i++){
-			 if(hondana[i] != null){
-					if (hon.name.equals(hondana[i].name) &&
-							 hon.price==(hondana[i].price) &&
-							 hon.author.equals(hondana[i].author)){
-				 System.out.println("同じ本の登録は出来ません。");
-				 tuika2();
-				 }
-		 }
-		 }
-		 hondana[x]=hon;
-		 x++;
-		 System.out.println("マイ本棚の"+x+"番目に追加完了しました。");
-		 if(x>=5){
-		 ippai();
-		 }else{
-		 tuika2();
-		 }
-}
-public void tuika2(){
-		System.out.println("続けて書籍を追加しますか？[はい: yes いいえ: no / 確認：ch]");
-			String tuika2=new java.util.Scanner(System.in).nextLine();
-			if(tuika2.equals("yes")){
-				tuika();
-			}else if(tuika2.equals("ch")){
-				hyoji();
-			}else{
-
+		Boolean isRegist = false;
+		for (int i = 0; i < hondana.length; i++) {
+			if (hondana[i] != null) {
+				if(hon.equals(hondana[i])){
+				/*if (hon.name.equals(hondana[i].name) &&
+					hon.price == (hondana[i].price) &&
+					hon.author.equals(hondana[i].author)) {*/
+					System.out.println ("同じ本の登録は出来ません。");
+					//tuika2();
+					isRegist = true;
+					break;
+				}
 			}
 		}
 
-public void hyoji(){
-		System.out.println("本を表示します。");
-		if(hondana[0] != null){
-		System.out.println(hondana[0]);
+		if (x == 5) {
+			 ippai();
+			 end();
+		} else {
+			if(!isRegist){
+			hondana[x] = hon;
+			x++;
+			System.out.println("マイ本棚の"+x+"番目に追加完了しました。");
+			}
+			tuika2();
+
 		}
-		if(hondana[1] != null){
-		System.out.println(hondana[1]);
-		}
-		if(hondana[2] != null){
-		System.out.println(hondana[2]);
-		}
-		if(hondana[3] != null){
-		System.out.println(hondana[3]);
-		}
-		if(hondana[4] != null){
-		System.out.println(hondana[4]);
-		}
-		System.out.println("===========================");
-		System.out.println("");
-		end();
 }
 
-public void ippai(){
-		System.out.println("これ以上追加できません。");
-		System.out.println(hondana[0]);
-		System.out.println(hondana[1]);
-		System.out.println(hondana[2]);
-		System.out.println(hondana[3]);
-		System.out.println(hondana[4]);
-		System.out.println("===========================");
-		System.out.println("");
-		end();
+public void tuika2() {
+		System.out.println ("続けて書籍を追加しますか？[はい: yes いいえ: no / 確認：ch]");
+			String tuika2 = new java.util.Scanner (System.in).nextLine();
+			if (tuika2.equals ("yes")) {
+				tuika();
+			} else if (tuika2.equals("ch")) {
+				hyoji();
+				end();
+			} else {
+				end();
+			}
+}
+
+public void hyoji() {
+		System.out.println ("本を表示します。");
+		System.out.println ("===========================");
+		if (hondana[0] != null) {
+		System.out.println (hondana[0]);
+		}
+		if (hondana[1] != null) {
+		System.out.println (hondana[1]);
+		}
+		if (hondana[2] != null) {
+		System.out.println (hondana[2]);
+		}
+		if (hondana[3] != null) {
+		System.out.println (hondana[3]);
+		}
+		if (hondana[4] != null) {
+		System.out.println (hondana[4]);
+		}
+		System.out.println ("===========================");
+		System.out.println ("");
+}
+
+public void ippai() {
+		System.out.println ("これ以上追加できません。");
+		System.out.println ("===========================");
+		System.out.println (hondana[0]);
+		System.out.println (hondana[1]);
+		System.out.println (hondana[2]);
+		System.out.println (hondana[3]);
+		System.out.println (hondana[4]);
+		System.out.println ("===========================");
+		System.out.println ("");
 		}
 }
