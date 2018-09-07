ae7f5f12aa2c8481943ac21bf9ab6ac2a608a20c
taro1210 <42398914+taro1210@users.noreply.github.com>
2018-09-07 15:15:37 +0900

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,23 @@
+package ExB1;
+
+public class Test2 {
+	public static void main(String[] args) {
+		int[] int1 = new int[] { 1, 2, 3 };
+		System.out.println(int1);
+
+		byte[] byte2 = new byte[] { 1, 2, 3 };
+		System.out.println(byte2);
+
+		double[] double3 = new double[] { 1.0, 2.0, 3.0 };
+		System.out.println(double3);
+
+		long[] long4 = new long[] { 1000000000, 2, 3 };
+		System.out.println(long4);
+
+		char[] char5 = new char[]{ 'こ', 'そ', 'あ', 'ど' };
+		System.out.println(char5);
+
+		String[] string6 = new String[]{ "あれ", "これ", "それ", "どれ" };
+		System.out.println(string6);
+	}
+}
