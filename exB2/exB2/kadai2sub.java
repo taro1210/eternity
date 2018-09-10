bacbfaffd4d8dae97b98abb049bea226dede7aea
taro1210 <42398914+taro1210@users.noreply.github.com>
2018-09-10 16:57:01 +0900

    Add files via upload

---
+++ exB2/exB2/kadai2sub.java
@@ -0,0 +1,65 @@
+package exB2;
+
+import java.io.StreamCorruptedException;
+import java.util.InputMismatchException;
+
+public class kadai2sub {
+
+	@SuppressWarnings("resource")
+	public int inputY() {
+		int year = 0;
+		// 年の入力
+		try {
+			String inputYear = new java.util.Scanner(System.in).nextLine();
+			if (inputYear.matches("0\\d+")) {
+				throw new StreamCorruptedException();
+			}
+			if (!inputYear.matches("\\d+")) {
+				throw new java.util.InputMismatchException();
+			}
+			year = Integer.parseInt(inputYear);
+			if (year < 1900) {
+				throw new StreamCorruptedException();
+			}
+			if (year > 2100) {
+				throw new StreamCorruptedException();
+			}
+			return year;
+		} catch (InputMismatchException e) {
+			System.out.println("文字うつなボケ");
+			return inputY();
+		} catch (StreamCorruptedException e) {
+			System.out.println("1900～2100の間で打たんかい");
+			return inputY();
+		}
+	}
+
+	@SuppressWarnings("resource")
+	public int inputM() {
+		int month = 0;
+		try {
+			String inputMonth = new java.util.Scanner(System.in).nextLine();
+			if (inputMonth.matches("0\\d+")) {
+				throw new StreamCorruptedException();
+			}
+			if (!inputMonth.matches("\\d+")) {
+				throw new java.util.InputMismatchException();
+			}
+			month = Integer.parseInt(inputMonth);
+			if (month < 1) {
+				throw new StreamCorruptedException();
+			}
+			if (month > 12) {
+				throw new StreamCorruptedException();
+			}
+			return month;
+		} catch (InputMismatchException e) {
+			System.out.println("文字うつなボケ");
+			return inputM();
+		} catch (StreamCorruptedException e) {
+			System.out.println("1～12の間で打たんかい");
+			return inputM();
+		}
+	}
+
+}
